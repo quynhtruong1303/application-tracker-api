0b6a6cfbe2c07407b9849579f3e08f33d566c0d3
Nha Truong <quynhtruong1303@gmail.com>
2026-04-29 00:31:30 -0600

    fix: add CORS middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import authRoutes from './routes/auth.js'
 import applicationRoutes from './routes/applications.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+
 app.use('/auth', authRoutes)
 app.use('/applications', applicationRoutes)
 
